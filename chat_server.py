3888f978d194707a9a7685cdc4ca6b613dd17a9d
Rajesh Korde <rajesh.korde@gmail.com>
2024-06-12 15:47:14 -0700

    full converted to simple conversation handler

--- chat_server.py
+++ chat_server.py
@@ -1,44 +1,32 @@
 import os
-import random
-from asyncio import CancelledError
-from email import message
-from tracemalloc import start
-from typing import Any, Dict
 
 from loguru import logger
 from telegram import (
-    KeyboardButton,
-    KeyboardButtonPollType,
-    Poll,
     ReplyKeyboardMarkup,
     ReplyKeyboardRemove,
     Update,
 )
-from telegram.constants import ParseMode
 from telegram.ext import (
     ApplicationBuilder,
-    CallbackContext,
     CommandHandler,
     ContextTypes,
     ConversationHandler,
     MessageHandler,
-    PollAnswerHandler,
-    PollHandler,
     filters,
 )
 
 from examgpt.frontend.chatbot.chat_helper import ChatHelper
 
-# chat = ChatHelper()
+chat = ChatHelper()
 
 
-# async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-#     await context.bot.send_message(
-#         chat_id=update.effective_chat.id, text="I'm an exam gpt bot, please talk to me"
-#     )
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id, text="Welcome to ExamGPT!"
+    )
 
-TOTAL_VOTER_COUNT = 3
-TOTAL_QUESTION_COUNT = 3
+
+TOTAL_QUESTION_COUNT = 2
 
 
 async def exam(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
@@ -56,38 +44,36 @@ async def exam(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await context.bot.send_message(chat_id=update.effective_chat.id, text=message)
 
 
-# async def longform(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-#     question = chat.longform()
-#     await context.bot.send_message(
-#         chat_id=update.effective_chat.id,
-#         text="\n".join([question.question, question.answer]),
-#     )
+async def longform(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    question = chat.longform()
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text="\n".join([question.question, question.answer]),
+    )
 
 
-# async def mutliple_choice(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-#     await context.bot.send_message(
-#         chat_id=update.effective_chat.id,
-#         text="I help with the multiple choice questions",
-#     )
+async def mutliple_choice(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text="I help with the multiple choice questions",
+    )
 
 
-# def start_chat2(token: str) -> None:
-#     logger.info("Starting Telegram Chatbot")
-#     application = ApplicationBuilder().token(token).build()
-#     start_handler = CommandHandler("start", start)
-#     exam_handler = CommandHandler("exam", exam)
-#     longform_handler = CommandHandler("qa", longform)
-#     multiple_choice_handler = CommandHandler("mc", mutliple_choice)
+def start_chat2(token: str) -> None:
+    logger.info("Starting Telegram Chatbot")
+    application = ApplicationBuilder().token(token).build()
+    start_handler = CommandHandler("start", start)
+    exam_handler = CommandHandler("exam", exam)
+    longform_handler = CommandHandler("qa", longform)
+    multiple_choice_handler = CommandHandler("mc", mutliple_choice)
 
-#     application.add_handler(start_handler)
-#     application.add_handler(exam_handler)
-#     application.add_handler(longform_handler)
-#     application.add_handler(multiple_choice_handler)
+    application.add_handler(start_handler)
+    application.add_handler(exam_handler)
+    application.add_handler(longform_handler)
+    application.add_handler(multiple_choice_handler)
 
-#     application.run_polling()
+    application.run_polling()
 
-
-## Polling code
 
 QUIZZING = 1
 
@@ -97,7 +83,7 @@ answer_markup_mc = ReplyKeyboardMarkup(answer_keyboard_mc, one_time_keyboard=Tru
 start_markup_mc = ReplyKeyboardMarkup(start_keyboard_mc, one_time_keyboard=True)
 
 
-async def start_chat_mc(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+async def start_mc(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
     """Start the conversation and ask user for input."""
 
     # Parse question count and topic here
@@ -113,23 +99,25 @@ async def start_chat_mc(update: Update, context: ContextTypes.DEFAULT_TYPE) -> i
             "last_answer": "X",
         }
     }
-    # Keep chat related data under chat_id key
+
     context.bot_data.update(chat_payload)
 
     question_str = "question" if question_count == 1 else "questions"  # type: ignore
-    reply_text = f"Ready for {question_count} multiple choice {question_str}?"
+    reply_text = f"Ready for {question_count} multiple choice {question_str}\n/cancel anytime to cancel quiz.?"
+
+    if not update.message:
+        logger.warning("Update does not have a message object")
+        return await cancel_mc(update, context)
+
     await update.message.reply_text(
         reply_text,
-        # reply_markup=ReplyKeyboardMarkup.from_button(
-        #     KeyboardButton("Start")
-        # ),
         reply_markup=start_markup_mc,
     )
 
     return QUIZZING
 
 
-async def ask_question_mc(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+async def quiz_mc(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
     """Start the conversation and ask user for input."""
 
     chat_id = update.effective_chat.id
@@ -139,7 +127,6 @@ async def ask_question_mc(update: Update, context: ContextTypes.DEFAULT_TYPE) ->
     logger.info(f"{chat_payload=}")
     last_answer = chat_payload["last_answer"]
     user_answer = update.effective_message.text
-    logger.info(f"{last_answer}")
 
     if not last_answer == "X":
         if user_answer == last_answer:
@@ -148,8 +135,6 @@ async def ask_question_mc(update: Update, context: ContextTypes.DEFAULT_TYPE) ->
             context.bot_data.update({chat_id: chat_payload})
         else:
             await update.message.reply_text("Incorrect!")
-
-    logger.info(f"last message: {update.effective_message.text}")
 
     if chat_payload["asked_question_count"] == chat_payload["total_question_count"]:
         return await completed_mc(update, context)
@@ -168,49 +153,6 @@ async def ask_question_mc(update: Update, context: ContextTypes.DEFAULT_TYPE) ->
     return QUIZZING
 
 
-# async def poll_handler_mc(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-#     """Handle answer to the poll"""
-
-#     logger.info(update)
-#     logger.info(update.poll)
-#     if update.poll.is_closed:
-#         return
-#     poll = update.poll
-#     poll_id = poll.id
-
-#     # need try catch here
-#     poll_data = context.bot_data.get(poll_id)
-#     if not poll_data:
-#         raise Exception("Could not find poll data")
-
-#     chat_id = poll_data["chat_id"]
-#     message_id = poll_data["message_id"]
-
-#     user_answer = [option.text for option in poll.options if option.voter_count > 0][0]
-#     correct_answer = poll.correct_option_id
-
-#     logger.info(f"{user_answer=}")
-#     logger.info(f"{correct_answer=}")
-
-#     logger.info(f"{update.poll.total_voter_count=}")
-#     logger.info(context.bot_data[chat_id])
-#     await context.bot.stop_poll(chat_id, message_id)
-
-
-# async def poll_answer_handler_mc(
-#     update: Update, context: ContextTypes.DEFAULT_TYPE
-# ) -> None:
-#     # user_answer = [option.text for option in poll.options if option.voter_count > 0][0]
-#     # correct_answer = poll.correct_option_id
-
-#     # logger.info(f"{user_answer=}")
-#     # logger.info(f"{correct_answer=}")
-#     chat_id = update.effective_chat.id
-
-#     update.poll_answer.option_ids
-#     update.poll_answer
-
-
 async def completed_mc(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
     chat_id = update.effective_chat.id
 
@@ -220,14 +162,14 @@ async def completed_mc(update: Update, context: ContextTypes.DEFAULT_TYPE) -> in
     total = chat_payload["total_question_count"]
     reply_text = f"You got {correct} out of {total} right!"
 
-    await update.message.reply_text(reply_text)
+    await update.message.reply_text(reply_text, reply_markup=ReplyKeyboardRemove())
 
     return ConversationHandler.END
 
 
 async def cancel_mc(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
     reply_text = "Quiz Cancelled."
-    await update.message.reply_text(reply_text)
+    await update.message.reply_text(reply_text, reply_markup=ReplyKeyboardRemove())
 
     return ConversationHandler.END
 
@@ -235,28 +177,23 @@ async def cancel_mc(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
 def main() -> None:
     token = os.environ["TG_BOT_TOKEN"]
 
-    # QUIZ_STARTED, QUIZZING = range(2)
-
     mc_handler = ConversationHandler(
-        entry_points=[CommandHandler("mc", start_chat_mc)],
+        entry_points=[CommandHandler("mc", start_mc)],
         states={
             QUIZZING: [
+                MessageHandler(filters.Regex("^(Cancel)$"), cancel_mc),
                 MessageHandler(
-                    filters.Regex("^Start|A|B|C|D$"),
-                    ask_question_mc,
-                )
+                    filters.Regex("^(Start|A|B|C|D)$") & ~filters.COMMAND,
+                    quiz_mc,
+                ),
             ]
         },
-        fallbacks=[CommandHandler("Cancel", cancel_mc)],
+        fallbacks=[CommandHandler("cancel", cancel_mc)],
     )
 
     application = ApplicationBuilder().token(token).build()
-    # application.add_handler(PollHandler(receive_quiz_answer))
-
     application.add_handler(mc_handler)
-    # application.add_handler(PollHandler(poll_handler_mc))
-    # application.add_handler(PollAnswerHandler(poll_answer_handler_mc))
-    # application.add_handler(PollHandler(handle_answer_mc))
+    application.add_handler(CommandHandler("start", start))
 
     logger.info("Starting App")
     application.run_polling(allowed_updates=Update.ALL_TYPES)
